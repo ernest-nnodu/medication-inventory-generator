83dc001993f7a1ccebae23f45597463dbff02f20
ernest-nnodu <ernest.nnodu@outlook.com>
2025-11-17 21:48:17 +0000

    Initial commit

---
+++ src/main/java/com/phoenixcode/medication/inventory/generator/MedicationInventoryGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.phoenixcode.medication.inventory.generator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedicationInventoryGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedicationInventoryGeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/phoenixcode/medication/inventory/generator/MedicationInventoryGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.phoenixcode.medication.inventory.generator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedicationInventoryGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
