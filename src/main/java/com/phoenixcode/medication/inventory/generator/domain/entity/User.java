426c07c7881ea6a07afeb371776eaf089dc536de
ernest-nnodu <ernest.nnodu@outlook.com>
2025-11-19 17:55:01 +0000

    Add user entity

---
+++ src/main/java/com/phoenixcode/medication/inventory/generator/domain/entity/User.java
@@ -0,0 +1,26 @@
+package com.phoenixcode.medication.inventory.generator.domain.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "users")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    UUID id;
+
+    String username;
+    String email;
+    String password;
+}
