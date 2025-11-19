2852515d0d0d578920130fd35f547eadec1dbd76
ernest-nnodu <ernest.nnodu@outlook.com>
2025-11-19 18:04:17 +0000

    Add resident entity

--- src/main/java/com/phoenixcode/medication/inventory/generator/domain/entity/Resident.java
+++ src/main/java/com/phoenixcode/medication/inventory/generator/domain/entity/Resident.java
@@ -1,4 +1,27 @@
 package com.phoenixcode.medication.inventory.generator.domain.entity;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "residents")
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
 public class Resident {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    private String firstName;
+    private String LastName;
+    private String serviceName;
+
 }
