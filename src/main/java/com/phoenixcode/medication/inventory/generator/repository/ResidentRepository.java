4fc4320da949ac7a130ce148d6dd474a804ccec9
ernest-nnodu <ernest.nnodu@outlook.com>
2025-11-20 20:57:08 +0000

    feat: add resident repository

---
+++ src/main/java/com/phoenixcode/medication/inventory/generator/repository/ResidentRepository.java
@@ -0,0 +1,11 @@
+package com.phoenixcode.medication.inventory.generator.repository;
+
+import com.phoenixcode.medication.inventory.generator.domain.entity.Resident;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface ResidentRepository extends JpaRepository<Resident, UUID> {
+}
