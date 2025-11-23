a4724e719459d6d803edbfc51f683dfc1e5caacd
ernest-nnodu <ernest.nnodu@outlook.com>
2025-11-23 15:09:38 +0000

    feat: Add medication service

---
+++ src/main/java/com/phoenixcode/medication/inventory/generator/service/impl/MedicationServiceImpl.java
@@ -0,0 +1,6 @@
+package com.phoenixcode.medication.inventory.generator.service.impl;
+
+import com.phoenixcode.medication.inventory.generator.service.MedicationService;
+
+public class MedicationServiceImpl implements MedicationService {
+}
