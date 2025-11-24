55c5b3edbd687e3e21b9bccdcd89eb3b44188865
ernest-nnodu <ernest.nnodu@outlook.com>
2025-11-24 21:27:30 +0000

    feat:Implement get residents endpoint in resident controller

--- src/main/java/com/phoenixcode/medication/inventory/generator/controller/ResidentController.java
+++ src/main/java/com/phoenixcode/medication/inventory/generator/controller/ResidentController.java
@@ -1,14 +1,29 @@
 package com.phoenixcode.medication.inventory.generator.controller;
 
+import com.phoenixcode.medication.inventory.generator.domain.dto.ResidentResponseDto;
 import com.phoenixcode.medication.inventory.generator.service.ResidentService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequestMapping(path = "/residents")
 public class ResidentController {
 
     private final ResidentService residentService;
 
     public ResidentController(ResidentService residentService) {
         this.residentService = residentService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ResidentResponseDto>> getResidents() {
+
+        List<ResidentResponseDto> residentResponseDtos = residentService.getAllResident();
+        return new ResponseEntity<>(residentResponseDtos, HttpStatus.OK);
     }
 }
