f11360cbda5631442516ecd4120320dc022178d0
ernest-nnodu <ernest.nnodu@outlook.com>
2025-11-30 14:59:34 +0000

    feat: Add resource not found exception class

---
+++ src/main/java/com/phoenixcode/medication/inventory/generator/Exception/ResidentNotFoundException.java
@@ -0,0 +1,8 @@
+package com.phoenixcode.medication.inventory.generator.Exception;
+
+public class ResidentNotFoundException extends RuntimeException {
+
+    public ResidentNotFoundException(String message) {
+        super(message);
+    }
+}
